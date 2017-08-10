786016709e639a9e680df0185467c2c9f696856d
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-08-10 11:07:33 +0900

    Practice1終了

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,6 +21,14 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
+		bo = true ;
+		by = 10;
+		s = 12;
+		c = 'あ';
+		i = 14;
+		l = 2345678989098l;
+		f = 3.457479f;
+		d = 2.34;
 
 
 
@@ -31,7 +39,14 @@ public class PTra01_07 {
 
 
 		// ★ それぞれの変数の中身を出力してください
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 
