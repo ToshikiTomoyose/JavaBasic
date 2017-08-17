47c213c3d61dad87205e4c9af016d2fd8c04df43
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-08-17 14:32:40 +0900

    pt3-6 clear

--- Practice/src/practice03/PTra03_06.java
+++ Practice/src/practice03/PTra03_06.java
@@ -8,11 +8,19 @@ package practice03;
 
 public class PTra03_06 {
 	public static void main(String[] args) {
+
 		int limitNumber = 100;
+
+		//java.util.Scanner scanner = new java.util.Scanner(System.in);
+		//System.out.println("テスト 入力してください。（数字のみ）");
+		//String line = scanner.nextLine();
 
 		// ランダムで数字を生成するプログラムです。
 		// 下記の命令を実行すると変数randomに、0以上、変数limitNumber未満の数字がランダムで代入されます
+
 		int random = new java.util.Random().nextInt(limitNumber + 1);
+
+		//int random = Integer.parseInt(line);
 
 		//---------------------ここから本題-----------------------
 		/*
@@ -25,6 +33,14 @@ public class PTra03_06 {
 		 *
 		 * ※ プログラムは何行書いても良いです
 		 */
-
+		if(random == 100) {
+			System.out.println("☆☆☆☆☆");
+		} else if(random <= 30) {
+			System.out.println("☆☆☆☆");
+		} else if (random >31 && random <=99 ) {
+			System.out.println("☆☆☆");
+		} else {
+			System.out.println("☆☆☆");
+		}
 	}
 }
