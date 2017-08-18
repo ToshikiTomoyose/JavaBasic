37b9bf5c120176b343267fe160984ed68eb1cca0
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-08-18 17:51:56 +0900

    pt4-3karaowari

--- Practice/src/practice04/PTra04_02.java
+++ Practice/src/practice04/PTra04_02.java
@@ -10,13 +10,14 @@ public class PTra04_02 {
 	public static void main(String[] args) {
 		// 通称「FizzBuzz問題」です
 		// ★ int型の変数indexを定義し、1で初期化してください
-
+			int index = 1;
 
 		// ★ 変数indexが100以下である場合繰り返す条件式を記述してください
-		while () {
+		while (true) {
+			if (index >= 100) {
 
 			// ★ 変数indexの値が3でも5でも割り切れる数の場合は、「FizzBuzz」と出力
-
+				
 
 			// ★ 変数indexの値が3で割り切れ、5で割り切れない場合は、「Fizz」と出力
 
