caebb180d39cdd8abc03abb2ea584fe5ecda3567
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-08-22 10:57:33 +0900

    saikoro douchu

--- Practice/src/practice04/PTra04_04.java
+++ Practice/src/practice04/PTra04_04.java
@@ -13,7 +13,7 @@ public class PTra04_04 {
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
 
 		// ★ int型の変数mapを宣言して、100で初期化してください
-			int map = 100;
+			int map = 120;
 
 		// ★ int型の変数progressを宣言して、0で初期化してください
 			int progress = 0;
@@ -22,7 +22,7 @@ public class PTra04_04 {
 		System.out.println("=========< START >==========");
 
 		// ★ 変数progressが変数mapの数より少ない限り繰り返しを行う条件式を記述してください
-		while () {
+		while (progress <= map) {
 
 			System.out.println("サイコロを振ります。Enterを押してください");
 
@@ -35,10 +35,11 @@ public class PTra04_04 {
 			dice = dice + 1;
 
 			// ★ サイコロを振って出た目を、変数progressに加算してください
-
+			progress = dice + progress;
 
 			// ★ 「残り##マス」と出力してください（##には、残りマスの数を入れてください）
-
+				System.out.print("残り");
+				System.out.println(progress + "マス");
 		}
 
 		System.out.println("=========< GOAL >==========");
