8ee57e5f6c08543079f568f47ec1ebb41e576415
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-08-25 18:20:55 +0900

    pt7 tothunyu

--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -35,13 +35,13 @@ public class PTra06_07 {
 			}
 			System.out.println(); // 改行
 		}
-				for (int l = 5 ; l > 0 ; l--) {  //←lは５で、０より大きい限り、
+				for (int l = 5 ; l > 0 ; l--) {  //←lは５で、０より大きい限り、１つずつ減らしていく
 				//System.out.print("□");
 
-				for (int m = 0 ; m < l ; m++) { //←■の数 lnでなくprint
+				for (int m = 0 ; m < l ; m++) { //←ｍは０で、ｍはｌより小さい限り、１つずつ増やしていく
 						System.out.print("■");
 					}
-				for (int n = 5 ; n > l ; n--) { //←■の数 lnでなくprint
+				for (int n = 5 ; n > l ; n--) { //←ｎは５。ｍはｌより大きい限り、ｎは１つずつ減らしていく
 						System.out.print("□");
 					}
 				System.out.println(); // 改行
