c2c9dfe107ebb8b70cef2e74503a92048b1fe3d7
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-08-28 12:23:44 +0900

    pt 7 -6made

--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -1,4 +1,7 @@
 package practice07;
+
+import java.util.Arrays;
+
 /*
  * PTra07_06.java
  *   作成	LIKEIT	2017
@@ -11,11 +14,24 @@ public class PTra07_06 {
 
 		// ★ PTra07_05.javaで作成したランダム値が入った配列arrayを作成してください
 
+		int[] array = new int[5];
 
-		System.out.println("配列の中身を小さい順に並び替えます");
+		for (int i = 0 ; i < array.length; i++) {
+		 	array[i] = i + 1;
+
+		 	System.out.println(array.length);
+
+		 	i = new java.util.Random(array.length).nextInt(100000);
+
+
+		 	System.out.println("配列の中身を小さい順に並び替えます");
+
 
 		// ★ 配列arrayの中身を、昇順(数値が小→大になるように)に並び替えてください
 		// 並び替えにはArrays.sort(array);を使用してくださいb。
 
+		 	Arrays.sort(array);
+		 	System.out.println(Arrays.toString(array));
+		}
 	}
 }
--- Practice/src/practice07/PTra07_07.java
+++ Practice/src/practice07/PTra07_07.java
@@ -10,12 +10,17 @@ public class PTra07_07 {
 	public static void main(String[] args) {
 
 		// ★ ※2次元配列を用いています
+		int[] i  = new int[3];
+		int[] j = new int [4];
+		
 		int[][] array = {
 				{500,40,6},
 				{70,8,90,1000}
 		};
+		System.out.println(array.length);
 
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
+
 
 	}
 }
