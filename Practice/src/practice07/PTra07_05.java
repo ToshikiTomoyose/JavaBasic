328ac60578ea668e6b6a6c4b8e364f73e8587fdf
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-08-28 10:55:29 +0900

    pt7-5 ??

--- Practice/src/practice07/PTra07_05.java
+++ Practice/src/practice07/PTra07_05.java
@@ -10,26 +10,26 @@ public class PTra07_05 {
 	public static void main(String[] args) {
 
 		// ★ int型の配列arrayを宣言して、5個の領域を確保してください
-		int[] array = new int[5];
+				int[] array = new int[5];
 
-		/*
-		 * ★ 下記の仕様でfor文を記述してください
-		 *
-		 * 初期化：変数iを宣言し、0で初期化してください
-		 * 条件式：変数iが、arrayの長さ未満である限り繰り返しを行います
-		 * 後処理：変数iに1を足して、変数iを上書きしてください
-		 */
-		//------for start
-		for (int i = 0 ; i < array.length; i++) {
-			 	array[i] = i + 1;
+				/*
+				 * ★ 下記の仕様でfor文を記述してください
+				 *
+				 * 初期化：変数iを宣言し、0で初期化してください
+				 * 条件式：変数iが、arrayの長さ未満である限り繰り返しを行います
+				 * 後処理：変数iに1を足して、変数iを上書きしてください
+				 */
+				//------for start
+				for (int i = 0 ; i < array.length; i++) {
+					 	array[i] = i + 1;
 
-			// ★ 0～100000の中からランダムで値を取得して、配列arrayのi番目に代入してください
-			 	 i = new java.util.Random(array.length).nextInt(100000);
+					// ★ 0～100000の中からランダムで値を取得して、配列arrayのi番目に代入してください
+					 	 i = new java.util.Random(array.length).nextInt(100000);
 
-			// ★ 配列arrayのi番目の値を出力してください
-			 	System.out.println(array[i]);
+					// ★ 配列arrayのi番目の値を出力してください
+					 	System.out.println(i);
 
-		//------for end
+				//------for end
+				}
+			}
 		}
-	}
-}
