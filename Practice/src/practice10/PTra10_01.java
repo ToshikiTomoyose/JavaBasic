33ad0e3f02d0802e61887a3ba0897496c0acc4e2
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-09-04 14:02:29 +0900

    pt 10 -2 clear ?

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -1,4 +1,5 @@
 package practice10;
+
 /*
  * PTra10_01.java
  *   作成	LIKEIT	2017
@@ -26,11 +27,12 @@ public class PTra10_01 {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
-
+		User us;
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
-
+		us = new User();
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
+		System.out.println(us.acopen());
 
 	}
 }
